ae68054b7c509200f3bc823e628f3020ea535e5d
arnaldotorr <arnaldo.torrealba@gmail.com>
2023-10-21 12:38:04 -0300

    Add styles

--- src/components/PokeApi.jsx
+++ src/components/PokeApi.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
 
 export const PokeApi = () => {
     const [selectedValue, setSelectedValue] = useState("type");
@@ -132,49 +133,67 @@ export const PokeApi = () => {
 
     return (
         <div>
-            <Form.Select
-                aria-label="Default select example"
-                onChange={handleFirstSelectChange}
-                style={{ width: "10rem" }}
-                value={selectedValue}
-            >
-                <option value="type">Tipo</option>
-                <option value="ability">Habilidad</option>
-            </Form.Select>
-            <Form.Select
-                aria-label="Default select example"
-                onChange={handleSecondSelectChange}
-                value={secondSelectedValue}
-                style={{ width: "10rem" }}
-            >
-                {pokeData ? (
-                    pokeData.map((item) => {
-                        return (
-                            <option key={item.name} value={item.url}>
-                                {item.name}
-                            </option>
-                        );
-                    })
-                ) : (
-                    <h1>Not found</h1>
-                )}
-            </Form.Select>
-            <Form.Select
-                aria-label="Default select example"
-                onChange={handleThirdSelectChange}
-                value={thirdSelectedValue}
-                style={{ width: "10rem" }}
-            >
-                <option>Seleccione:</option>
-                <option value="name">Nombre</option>
-                <option value="weight">Peso</option>
-                <option value="height">Altura</option>
-            </Form.Select>
+            <h1>Listado Pokemon por Tipo y Habilidad</h1>
+            <div className="poke-filter">
+                <FloatingLabel controlId="floatingSelect" label="Filtre por:">
+                    <Form.Select
+                        aria-label="Floating label select example"
+                        onChange={handleFirstSelectChange}
+                        style={{ width: "10rem" }}
+                        value={selectedValue}
+                    >
+                        <option value="type">Tipo</option>
+                        <option value="ability">Habilidad</option>
+                    </Form.Select>
+                </FloatingLabel>
+                <FloatingLabel
+                    controlId="floatingSelect"
+                    label={`Filtre ${
+                        selectedValue === "type" ? "Tipo" : "Habilidad"
+                    }`}
+                >
+                    <Form.Select
+                        aria-label="Floating label select example"
+                        onChange={handleSecondSelectChange}
+                        value={secondSelectedValue}
+                        style={{ width: "10rem" }}
+                    >
+                        {pokeData ? (
+                            pokeData.map((item) => {
+                                return (
+                                    <option key={item.name} value={item.url}>
+                                        {item.name}
+                                    </option>
+                                );
+                            })
+                        ) : (
+                            <h1>Not found</h1>
+                        )}
+                    </Form.Select>
+                </FloatingLabel>
+                <FloatingLabel controlId="floatingSelect" label="Ordene por:">
+                    <Form.Select
+                        aria-label="Floating label select example"
+                        onChange={handleThirdSelectChange}
+                        value={thirdSelectedValue}
+                        style={{ width: "10rem" }}
+                    >
+                        <option>Seleccione</option>
+                        <option value="name">Nombre</option>
+                        <option value="weight">Peso</option>
+                        <option value="height">Altura</option>
+                    </Form.Select>
+                </FloatingLabel>
+            </div>
             <div className="poke-container">
                 {pokeList ? (
                     pokeList.map((item) => {
                         return (
-                            <Card key={item.name} style={{ width: "15rem" }}>
+                            <Card
+                                key={item.name}
+                                className="bg-dark text-white"
+                                style={{ width: "15rem" }}
+                            >
                                 <Card.Img variant="top" src={item.img} />
                                 <Card.Body>
                                     <Card.Title>{item.name}</Card.Title>
